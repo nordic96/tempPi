38d31d8ccd945f96de19a6c11541868be5f80e0d
nordic96 <rhrlgns96@gmail.com>
2018-12-15 00:13:23 +0800

    added mysql insertion function

--- tempPi.py
+++ tempPi.py
@@ -5,6 +5,8 @@ import time
 import math
 import datetime
 import RPi.GPIO as GPIO
+import MySQLdb
+
 
 LED = 7 #pin 7
 GPIO.setwarnings(False)
@@ -19,8 +21,11 @@ A2 = 0x42
 A3 = 0x43
 bus = smbus.SMBus(1)
 record = 0.0
-f = open('temprec.txt', 'w')
+f = open('temprec.txt', 'a')
+db = MySQLdb.connect("localhost", "root", "nordic96", "test")
 while True:
+    cursor = db.cursor()
+
     GPIO.output(LED, GPIO.HIGH)
     time.sleep(0.5)
     bus.write_byte(address, A0)
@@ -30,10 +35,18 @@ while True:
         continue
     else:
         now = datetime.datetime.now()
+        #mysql insertion
+        sql = "insert into TempRecord(date, temp) values( %s, %s)"
+        try:
+            cursor.execute(sql, (now.strftime('%Y-%m-%d %H:%M:%S'), temp))
+        except:
+            db.rollback()
+        db.commit()
         record = str(now) + ' Temp: %3.3f' %temp
         print record
         f.write(record + '\n')
         GPIO.output(LED, GPIO.LOW)
         time.sleep(1.5)
     record = temp
+db.close()
 f.close()
