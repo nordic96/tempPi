6fc331901bcb705adb84a5a4ace33169fe0b4024
nordic96 <rhrlgns96@gmail.com>
2018-12-22 15:35:49 +0800

    updated measuring with average value

--- tempPi.py
+++ tempPi.py
@@ -11,6 +11,26 @@ import lcd_16x2 as LCD
 import buzzer
 
 start = datetime.now()
+LED = 4 #pin 7/GPIO 4
+GREEN = 17
+RED = 27
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+GPIO.setup(GREEN, GPIO.OUT)
+GPIO.setup(RED, GPIO.OUT)
+
+address = 0x48
+A0 = 0x40
+A1 = 0x41
+A2 = 0x42
+A3 = 0x43
+bus = smbus.SMBus(1)
+record = 0.0
+prevAverage = 0.0
+#f = open('temprec.txt', 'a')
+db = MySQLdb.connect("localhost", "root", "nordic96", "test")
 
 def tempcheck(temp):
     if temp >= 31:
@@ -20,8 +40,9 @@ def tempcheck(temp):
         GPIO.output(RED, GPIO.LOW)
         GPIO.output(GREEN, GPIO.HIGH)
 
-def dbInsertValidate(now, startTime, timegap):
-    if now >= startTime + timedelta(seconds=timegap):
+def dbInsertValidate(now, startTime, timegap, temp):
+    LCD.lcd_init()
+    if now >= startTime + timedelta(minutes=timegap):
         #mysql insertion
         sql = "insert into TempRecord(rec_year, rec_month, rec_date, rec_time, temp) values(%s, %s, %s, %s, %s)"
         # print sql %(now.year, now.month, now.day, now.strftime('%H:%M:%S'), temp)
@@ -35,35 +56,19 @@ def dbInsertValidate(now, startTime, timegap):
             time.sleep(2)
             db.rollback()
         db.commit()
+
         global start
         start = now
 
+def printLog(now, temp):
+        log = str(now) + ' Temp: %3.3f' %temp
+        LCD.lcd_string('Temp: %3.2f C' %temp, 1)
+        LCD.lcd_string('Time: %s' %now.strftime('%H:%M:%S'), 2)
+        print log
 
 if __name__=='__main__':
-    LED = 4 #pin 7/GPIO 4
-    GREEN = 17
-    RED = 27
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LED, GPIO.OUT)
-    GPIO.output(LED, GPIO.LOW)
-    GPIO.setup(GREEN, GPIO.OUT)
-    GPIO.setup(RED, GPIO.OUT)
-
-    address = 0x48
-    A0 = 0x40
-    A1 = 0x41
-    A2 = 0x42
-    A3 = 0x43
-    bus = smbus.SMBus(1)
-    record = 0.0
-    #f = open('temprec.txt', 'a')
-    db = MySQLdb.connect("localhost", "root", "nordic96", "test")
-
     #init lcd screen
     LCD.lcd_init()
-
-
     try:
         while True:
             cursor = db.cursor()
@@ -72,25 +77,34 @@ if __name__=='__main__':
             bus.write_byte(address, A0)
             value = bus.read_byte(address)
             temp = (float)(value) * 330/255
+            average = 0.0
+            if(record == 0.0):
+                temp = temp
+            else:
+                average = (temp + record)/2
+
             tempcheck(temp)
 
             if record == temp:
                 continue
             else:
-                buzzer.buzz(0.1)
                 now = datetime.now()
 
-                dbInsertValidate(now, start, 10)
-
-                record = str(now) + ' Temp: %3.3f' %temp
-                LCD.lcd_string('Temp: %3.2f C' %temp, 1)
-                LCD.lcd_string('Time: %s' %now.strftime('%H:%M:%S'), 2)
-                print record
+                if(prevAverage == 0.0):
+                    dbInsertValidate(now, start, 10, temp)
+                    printLog(now, temp)
+                else:
+                    if(prevAverage == average):
+                        continue
+                    else:
+                        dbInsertValidate(now, start, 10, average)
+                        printLog(now, average)
+                prevAverage = average
                 GPIO.output(LED, GPIO.LOW)
                 time.sleep(2)
             record = temp
     except KeyboardInterrupt:
-        pass
+       GPIO.cleanup()
     finally:
         db.close()
-        GPIO.cleanup()
+
